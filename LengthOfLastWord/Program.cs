232f231fc687dc76571fa653ff91ac0e2687ceb9
Mika Mirzanasiri <mika-mm@hotmail.com>
2023-02-21 16:07:59 +0000

    Add project files.

---
+++ LengthOfLastWord/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Solution.LengthOfLastWord("luffy is still joyboy"));
+    }
+}
+
+public class Solution
+{
+    public static int LengthOfLastWord(string s)
+    {
+        string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return words[^1].Length;
+    }
+}
